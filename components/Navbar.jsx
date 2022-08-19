9212baf2114fe0c143312d796d7de7c6ba4c27eb
unknown <bbaska837@gmail.com>
2022-08-19 10:01:03 +0800

    navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import css from "../styles/Navbar.module.scss";
+const Navbar = () => {
+  return (
+    <div className={css.Navbar}>
+      <Link href="1234.mn">
+        <a className={css.link}>
+          <img src="logo.jpg" alt="" /> 1234.mn
+        </a>
+      </Link>
+      <Link href="1234.mn">
+        <a className={css.link1}>Нүүр</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
